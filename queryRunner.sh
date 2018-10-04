6b347ae387d75944850d56486239337b0cadc408
Chen Yang <chen@pporn.tv>
2018-10-04 11:40:52 -0500

    update queryRunner

--- queryRunner.sh
+++ queryRunner.sh
@@ -9,17 +9,19 @@ echo ===========================================
 echo             Load Data to AuctionBase.db 
 echo ===========================================
 # Load data into database
-rm AuctionBase.db
 sqlite3 AuctionBase.db < create.sql
 sqlite3 AuctionBase.db < load.txt
 
 echo ===========================================
 echo              Run Queries
 echo ===========================================
+# Process all queries
 for query in ./query*.sql
 do
-    sqlite3 AuctionBase.db < $query
+    sqlite3 AuctionBase.db < $query >> ./output.txt
 done
+
+cat ./output.txt
 
 echo ===========================================
 echo              Clean Up
@@ -27,3 +29,4 @@ echo ===========================================
 # Clean up
 rm *.dat
 rm AuctionBase.db
+rm ./output.txt
