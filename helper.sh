90388857181006d698c3aaba00abc5feb000165b
Chen Yang <chen@pporn.tv>
2018-10-04 11:13:33 -0500

    integrate utility scripts into queryRunner.sh

--- helper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm AuctionBase.db
-
-sqlite3 AuctionBase.db < create.sql
-sqlite3 AuctionBase.db < load.txt
--- queryRunner.sh
+++ queryRunner.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
+echo ===========================================
+echo             Parsing Json Files            
+echo ===========================================
+sh ./runParser.sh
+
+echo ===========================================
+echo             Load Data to AuctionBase.db 
+echo ===========================================
+# Load data into database
+rm AuctionBase.db
+sqlite3 AuctionBase.db < create.sql
+sqlite3 AuctionBase.db < load.txt
+
+echo ===========================================
+echo              Run Queries
+echo ===========================================
 for query in ./query*.sql
 do
     sqlite3 AuctionBase.db < $query
 done
+
+echo ===========================================
+echo              Clean Up
+echo ===========================================
+# Clean up
+rm *.dat
+rm AuctionBase.db
